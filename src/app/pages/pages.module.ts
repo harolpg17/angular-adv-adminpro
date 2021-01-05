55bec480e590879fed88441e7e19813f5da99cf2
Harol Perez Giraldo <harolpg17@hotmail.com>
2021-01-05 16:11:38 -0500

    Configuración iniciar progress bar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+
 import { PagesComponent } from './pages.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
-
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { AppRoutingModule } from '../app-routing.module';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
     AppRoutingModule
   ], exports: [
